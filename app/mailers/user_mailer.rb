176d66ddae0060df51887e861a8e4fbfd80827da
anhcvt-2280 <vu.tuan.anhc@sun-asterisk.com>
2020-08-17 14:09:02 +0700

    Chapter 12 - Password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,5 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t("mail.head")
   end
 
-  def password_reset; end
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_resets.new.title_reset_passoword")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validations.users.email_regex
   USERS_PARAMS_PERMIT = %i(name email password password_confirmation).freeze
+  PASSWORD_PARAMS_PERMIT = %i(password password_confirmation).freeze
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   validates :name, presence: true,
             length: {maximum: Settings.validations.users.name_max_length}
@@ -56,6 +57,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.validations.users.expire_reset_pass.hours.ago
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
   end
 
   private
